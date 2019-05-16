9044b92021a6665fe01d72767d26553d416c8f3c
yanlibo2013 <yanlibo2010@163.com>
2019-05-16 12:52:05 +0800

    add file lang

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import i18n from './lang' // Internationalization
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import './styles/element-variables.scss'
+
+import './styles/index.scss' // global css
+
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './icons' // icon
+
+
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
 
 Vue.config.productionTip = false;
 
@@ -12,4 +31,5 @@ new Vue({
   router,
   components: { App },
   template: '<App/>',
+  i18n,
 });
