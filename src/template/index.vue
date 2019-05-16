54a19dbc0f7d30a20b5fe6e128611bb9344cc525
yanlibo2013 <yanlibo2010@163.com>
2019-05-16 09:49:57 +0800

    add template file

---
+++ src/template/index.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  components: {},
+  data: function() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed: function() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+</style>
